93f71283170c44a80ecd974e7e9b35771ab82e80
FRITZ IRONHACK <weninger.fritz@gmail.com>
2023-12-19 10:38:48 +0100

    done

--- README.md
+++ README.md
@@ -1,32 +1,39 @@
-#CosmoHub
+# CosmoHub
 
-
-##Descripción
+## Descripción
 CosmoHub es un proyecto estudiantil que explora las maravillas del espacio exterior al aprovechar datos de dos poderosas APIs: NASA y SpaceX. Este proyecto ofrece una experiencia única, combinando imágenes cautivadoras del Astronomy Picture of the Day (APOD) de la NASA con información en tiempo real sobre los lanzamientos de SpaceX.
 
-##Características
-Integración de NASA APOD: Visualiza la Imagen Astronómica del Día con información detallada sobre cada imagen.
-Información de Lanzamientos de SpaceX: Mantente actualizado sobre los últimos lanzamientos de SpaceX, incluido el estado de éxito y detalles sobre los cohetes.
-Visualización Combinada: Experimenta una fusión perfecta de impresionantes imágenes de la NASA y datos de lanzamientos de SpaceX en una sola plataforma.
-Tecnologías Utilizadas
-Frontend: HTML, CSS, JavaScript
-APIs: NASA APOD API, SpaceX API
-Cómo Usar
-Clona el repositorio en tu máquina local.
-Abre el archivo index.html en tu navegador web.
-Explora las maravillas del espacio con la imagen diaria del APOD y los detalles de los lanzamientos de SpaceX.
-Estructura del Proyecto
-index.html: El archivo HTML principal del proyecto.
-styles.css: Archivo CSS para el estilo del proyecto.
-script.js: Archivo JavaScript para obtener y mostrar datos de las APIs de NASA y SpaceX.
-Colaboradores
-[Fritz]
+![alt text](https://ih1.redbubble.net/image.4680922817.0373/ur,pin_small_front,square,1000x1000.jpg)
 
-##Agradecimientos
-Andrés e Iginio
-API de NASA: [https://images-api.nasa.gov]
-API de SpaceX: [https://api.spacexdata.com/v5/launches/]
+Try this markdown:
+
+![alt text](http://akpsi-umdearborn.org/wp-content/uploads/2019/03/spacex-logo.jpg)
 
 
+## Características
+- **Integración de NASA APOD:** Visualiza la Imagen Astronómica del Día con información detallada sobre cada imagen.
+- **Información de Lanzamientos de SpaceX:** Mantente actualizado sobre los últimos lanzamientos de SpaceX, incluido el estado de éxito y detalles sobre los cohetes.
+- **Visualización Combinada:** Experimenta una fusión perfecta de impresionantes imágenes de la NASA y datos de lanzamientos de SpaceX en una sola plataforma.
 
 
+## Tecnologías Utilizadas
+- **Frontend:** HTML, CSS, JavaScript
+- **APIs:** [NASA APOD API](https://images-api.nasa.gov), [SpaceX API](https://api.spacexdata.com/v5/launches/)
+
+## Cómo Usar
+1. Clona el repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+3. Explora las maravillas del espacio con la imagen diaria del APOD y los detalles de los lanzamientos de SpaceX.
+
+## Estructura del Proyecto
+- `index.html`: El archivo HTML principal del proyecto.
+- `styles.css`: Archivo CSS para el estilo del proyecto.
+- `script.js`: Archivo JavaScript para obtener y mostrar datos de las APIs de NASA y SpaceX.
+
+## Colaboradores
+- [Fritz]
+
+## Agradecimientos
+- Andrés e Iginio
+- **API de NASA:** [https://images-api.nasa.gov]
+- **API de SpaceX:** [https://api.spacexdata.com/v5/launches/]
--- script.js
+++ script.js
@@ -1,58 +1,10 @@
-// "https://api.spacexdata.com/v4/launches"
-// fetch("https://api.spacexdata.com/v4/launches")
-// .then((response) => {
-//   return response.json();
-// })
-// .then((data) => {
-//   console.log("Parsed response: ", data);
-// })
-// .catch( (err) => console.log(err));
 
-//paso 1 API de Spacex funcion
-// api nasa sigue funcionando
-// ver peticiones restantes NASA DONE-
-// peticion en una funcion --- PENDING
-//llamar a esa funcion con un boton que muestre todos los datos en el daily - EXTRA
-// 2º API NASA || API SPACEX ( por mi cuenta || ironhack)
-// API NASA IMAGENES 13.25 ----- para las 14.00 tener renderizadas las imagenes o algo
-//paso 2 Promise.all, console.log
 
 const apiKey = "6MpG3Z3lsOARafqeDDhDtJyJO5aqkDJ85MWSZfK3";
 const apiUrl1 = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 const apiUrl2 = 'https://images-api.nasa.gov/search?q=moon';
 const apiUrl3 = "https://api.spacexdata.com/v5/launches/"
-// function showDaily() {
-//   fetch(apiUrl1)
-//     .then((response) => {
-//       console.log("Todos los encabezados:", response.headers);
-//       // Obtener el valor específico de un encabezado
-//       const rateLimitLimit = response.headers.get("X-RateLimit-Limit");
-//       const rateLimitRemaining = response.headers.get("X-RateLimit-Remaining");
 
-//       // Imprimir los valores en la consola
-//       console.log("X-RateLimit-Limit:", rateLimitLimit);
-//       console.log("X-RateLimit-Remaining:", rateLimitRemaining);
-
-//       // Verificar si la solicitud fue exitosa (código de estado 200)
-
-//       if (!response.ok) {
-//         throw new Error(`Error en la solicitud: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       // Insertar datos en el HTML
-//       console.log("datos obtenidos:", data);
-//       document.getElementById("daily-title").textContent = data.title;
-//       document.getElementById("date").textContent = `Fecha: ${data.date}`;
-//       document.getElementById("explanation").textContent = data.explanation;
-//       document.getElementById("image").src = data.url;
-//     })
-//     .catch((error) => {
-//       // Manejar errores en caso de que la solicitud no sea exitosa
-//       console.error("Error al obtener los datos:", error);
-//     });
-// }
 
 Promise.all([
   fetch(apiUrl1).then(response => response.json()),
@@ -74,35 +26,20 @@ Promise.all([
       </div>
     
     </article>
-      `
-        
-       
+      `  
       )
-
       const main = document.querySelector(".principal")
-
       main.append(article)
     })
-    
-
-    // Manipular los datos de la respuesta de la API APOD
+    // DOM API APOD
 
     console.log("Datos de APOD:", apodData);
     document.getElementById("daily-title").textContent = apodData.title;
     document.getElementById("date").textContent = `Fecha: ${apodData.date}`;
-    document.getElementById("explanation").textContent = apodData.explanation;
+    //document.getElementById("explanation").textContent = apodData.explanation;
     document.getElementById("image").src = apodData.url;
-    
-   
-    // document.getElementById("insertarTexto").textContent = imagesData.collection.items
-    // Manipular los datos de la respuesta de la API de imágenes
-
-    // Puedes agregar aquí la lógica para mostrar los resultados de la segunda API
-
-    // console.log( "datos items:", imagesData.collection.items)
-;
   })
   .catch(error => {
-    // Manejar errores en caso de que alguna de las solicitudes no sea exitosa
+   
     console.error('Error al obtener los datos:', error);
   });
